8b8d6950b427131dfda7fc15678b14803996ef03
pph2jd5xh6 <pph2jd5xh6@privaterelay.appleid.com>
2024-10-20 16:02:05 +0000

    修改

--- src/ustils/request.tsx
+++ src/ustils/request.tsx
@@ -1,38 +1,41 @@
-import axios from 'axios'
-import {requestTimeout,baseURL} from './constant'
+import axios from "axios";
+import { requestTimeout, baseURL } from "./constant";
 
-
-const handleResult=(response:any)=>{
-  console.log(response,"responseresponse");
-  
-}
+const handleResult = (response: any) => {
+  console.log(response, "responseresponse");
+};
 
 //创建实例
 const instance = axios.create({
   baseURL: baseURL,
   timeout: requestTimeout,
-  headers: {'X-Custom-Header': 'foobar'} 
-})
+  headers: { "X-Custom-Header": "foobar" },
+});
 
 // 添加请求拦截器
-instance.interceptors.request.use(function (config) {
-  instance.defaults.headers.aaa="bbbb" //自定义请求头
-  // 在发送请求之前做些什么
-  return config;
-}, function (error) {
-  // 对请求错误做些什么
-  return Promise.reject(error);
-});
+instance.interceptors.request.use(
+  function (config) {
+    instance.defaults.headers.aaa = "bbbb"; //自定义请求头
+    // 在发送请求之前做些什么
+    return config;
+  },
+  function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  }
+);
 
 // 添加响应拦截器
-instance.interceptors.response.use(function (response) {
-  const result=handleResult(response)
-  
-  return response;
-}, function (error) {
-  // 对响应错误做点什么
-  return Promise.reject(error);
-});
+instance.interceptors.response.use(
+  function (response) {
+    const result = handleResult(response);
 
-export default instance
+    return response;
+  },
+  function (error) {
+    // 响应错误
+    return Promise.reject(error);
+  }
+);
 
+export default instance;
